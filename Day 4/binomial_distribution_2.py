183bdfce7ecd2b864902ae5029004cb55df52d56
iamabhaytiwari343 <108920327+iamabhaytiwari343@users.noreply.github.com>
2023-07-11 22:07:40 +0530

    binomial and geometric distribution

--- Day 4/binomial_distribution_2.py
+++ Day 4/binomial_distribution_2.py
@@ -6,8 +6,8 @@ def binomial_prob(n, p, x):
 reject_percent, n = map(float, '12 10'.split())
 p = reject_percent/100
 b = 0
-for x in range(0, 3):  # x=0,1,2
+for x in range(0, 3):
     b += binomial_prob(n, p, x)
 print("%.3f" % b)
 print(round(sum([binomial_prob(n, p, i)
-      for i in range(2, 11)]), 3))  # x=2,3,4,...,10
+      for i in range(2, 11)]), 3))
