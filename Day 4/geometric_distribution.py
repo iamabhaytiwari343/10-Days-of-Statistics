66faf01b65cc4299207f81a3fc0283d67ae7ad59
iamabhaytiwari343 <108920327+iamabhaytiwari343@users.noreply.github.com>
2023-07-11 22:07:06 +0530

    binomial and geometric distribution

--- Day 4/geometric_distribution.py
+++ Day 4/geometric_distribution.py
@@ -1,7 +1,6 @@
 def geometric_prob(p, x):
     g = (1-p)**(x-1) * p
     return(g)
-
 numerator, denominator = map(float, input().split())
 x = int(input())
 p = numerator/denominator
--- Day 4/geometric_distribution2.py
+++ Day 4/geometric_distribution2.py
@@ -1,11 +1,10 @@
 def geometric_prob(p, x):
     g = (1-p)**(x-1) * p
     return(g)
-
 numerator, denominator = map(float, input().split())
 x = int(input())
 p = numerator/denominator
 g = 0
-for i in range(1,6): # i=1,2,3,4,5
+for i in range(1,6):
     g += geometric_prob(p, i)   
 print("%.3f" %g)
